ecae1847f74fc0e881a0ca288322d7e1c63e5935
Rosalia Dina Marina S <rosalia03rrrbkl@gmail.com>
2023-11-20 13:16:19 +0700

    Modifikasi Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,25 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\SiswaController;
 use App\Models\Author;
 use App\Models\book;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/mahasiswa/{npm}', [MahasiswaController::class, 'view']);
 Route::get('/siswa', [SiswaController::class, "siswa"]);
@@ -30,4 +42,27 @@ Route::get('/author/{author}', [AuthorController::class, 'show'])->name('author.
 Route::get('/author/{author}/edit', [AuthorController::class, 'edit'])->name('author.edit');
 Route::put('/author/{author}', [AuthorController::class, 'update'])->name('author.update');
 Route::delete('/author{author}', [AuthorController::class,'destroy'])->name('author.destroy');
-// Route::resource('author', [AuthorController::class]);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/layouts', function(){
+    return view('layouts.material');
+});
+Route::get('/contoh-a', function(){
+        return view('contoh.contoh-a');
+ 
+});
+
+require __DIR__.'/auth.php';
